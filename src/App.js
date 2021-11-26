58fb75debbb4e845313a944fe15401c7bf618097
Subashini <subashini3910@gmail.com>
2021-11-26 14:55:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Cform from './Cform';
+function App() {
+  return (
+    <div>
+     <br/>
+     <h2 className="App">FEEDBACK FORM</h2>
+    
+     <br/>
+    <Cform />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Cform.js
@@ -0,0 +1,145 @@
+import React,{Component} from 'react';
+
+
+class Cform extends Component{
+    constructor(){
+        super();
+        this.state={
+            name:'',
+            phonenumber:'',
+            email:'',
+            points:"",
+            textarea:"",
+            errors:{
+                name:'',
+                phonenumber:'',
+                email:'',
+                points:"",
+                textarea:"",
+            }
+        };
+    }
+
+    handlesubmit = async (e) => {
+        e.preventDefault();
+       
+        var errKeys = await Object.keys(this.state).filter((key) => {
+          if (this.state[key] == '' && this.state[key] != this.state.errors[key]) {
+            return key;
+          }
+        });
+       
+        if (errKeys.length >= 1) alert('Please fill all fields');
+        else console.log(this.state);
+       
+      };
+      
+    
+      
+
+      validation_form(e,val) {
+          if(e==='phonenumber')
+        {
+         for(var i=0;i<val.length;i++)
+         {
+             if(!(val[i] >= '0' && val[i] <= '9'))
+             {
+        
+                 break;
+              
+             }
+        }
+            if(i===val.length)
+            {
+              return true;
+            }else{
+                return false;
+            }
+        }
+
+        if(e==='email')
+        {
+            let pattern = /^([a-zA-Z0-9_.])+\@([a-z])+\.([a-z])+$/;
+            if(pattern.test(val))
+            {
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+     }
+
+    handlechange = async(e)=>{
+        var errors1 = { ...this.state.errors };
+        
+        if (e.target.value === '') {
+          errors1[e.target.name] = 'Required';
+        } else {
+            var check = this.validation_form(e.target.name,e.target.value)
+            if(check === false){
+                errors1[e.target.name] = 'Please enter valid ' + e.target.name;
+            }else{
+          errors1[e.target.name] = '';
+            }
+        } 
+        
+    
+        
+      await this.setState({errors:errors1,[e.target.name]: e.target.value });
+        console.log(this.state);
+    }
+
+    render(){
+        return(
+            <>
+            <form onSubmit={this.handlesubmit}>
+                <div>
+                    <label>Name</label><br/>
+                    <input type='text' name='name' value={this.state.name} onChange={this.handlechange}/>
+                    <br/>
+                <span style={{ color: 'red' }}>{' '}{this.state.errors.name}{' '}</span>
+                </div>
+                <br />
+                <div>
+                    <label>Email</label><br/>
+                    <input type='text' name='email' value={this.state.email}  onChange={this.handlechange} />
+                    <br/>
+                <span style={{ color: 'red' }}>{' '}{this.state.errors.email}{' '}</span>
+                </div>
+                <br/>
+                <div>
+                    <label>Phonenumber</label> <br/>
+                    <input type='text' name='phonenumber' maxlength='10' value={this.state.phonenumber}  onChange={this.handlechange}/>
+                    <br/>
+                <span style={{ color: 'red' }}>{' '}{this.state.errors.phonenumber}{' '}</span>
+                </div>
+                <br/>
+                <div>
+                   <label>Points</label> <br/><br/>
+                   <label><input type="radio" name="points" /> 1</label>&nbsp;&nbsp;
+                   <label><input type="radio" name="points" /> 2</label>&nbsp;&nbsp;
+                   <label><input type="radio" name="points" /> 3</label>&nbsp;&nbsp;
+                   <label><input type="radio" name="points" /> 4</label>&nbsp;&nbsp;
+                   <label><input type="radio" name="points" /> 5</label>
+
+                   <br/>
+                <span style={{ color: 'red' }}>{' '}{this.state.errors.points}{' '}</span>
+                </div><br/>
+                <br/>
+                <br/>
+                <div>
+                    <label >Textarea</label><br/>
+                   <textarea name="textarea" value={this.state.textarea} onChange={this.handlechange}></textarea>
+                   <span style={{ color: 'red' }}>{' '}{this.state.errors.textarea}{' '}</span>
+                </div>
+                <br/>
+                <button>Submit</button>
+               
+            </form>
+            </>
+        )
+    }
+}
+
+export default Cform;
